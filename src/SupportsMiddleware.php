0a19aa796df1ae3a63a4ac6fb8fd27daf65d3e78
Jesse Gall <dev@jessegall.nl>
2025-10-18 19:14:33 +0200

    Corrected ClassType in doc block

--- src/SupportsMiddleware.php
+++ src/SupportsMiddleware.php
@@ -5,7 +5,7 @@ namespace JesseGall\InertiaOverlay;
 interface SupportsMiddleware
 {
 
-    /** @return class-string<SupportsMiddleware>[] */
+    /** @return class-string<OverlayMiddleware>[] */
     public static function middleware(): array;
 
 }
