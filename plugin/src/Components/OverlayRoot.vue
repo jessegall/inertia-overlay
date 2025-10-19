5342f0b1e310b59831662aa3ddc1124650a554bd
Jesse Gall <dev@jessegall.nl>
2025-10-19 23:41:44 +0200

    Smooth backdrop blur

--- plugin/src/Components/OverlayRoot.vue
+++ plugin/src/Components/OverlayRoot.vue
@@ -15,9 +15,22 @@ function closeOverlay(overlayId: string) {
     useOverlay(overlayId)?.close();
 }
 
-function shouldBlur(overlayId: string): boolean {
-    return overlayId == stack.value[stack.value.length - 1]
-        && useOverlay(overlayId).hasStatus('opening', 'open')
+function shouldBlurBackground(overlayId: string): boolean {
+    if (stack.value.length === 1) {
+        return overlayId == stack.value[stack.value.length - 1]
+            && useOverlay(overlayId).hasStatus('opening', 'open')
+    }
+
+    const secondLastOverlayId = stack.value[stack.value.length - 2];
+    if (secondLastOverlayId === overlayId) {
+        return useOverlay(stack.value[stack.value.length - 1]).hasStatus('opening', 'closing');
+    }
+
+    if (overlayId === stack.value[stack.value.length - 1]) {
+        return useOverlay(overlayId).hasStatus('open');
+    }
+
+    return false;
 }
 
 </script>
@@ -27,7 +40,7 @@ function shouldBlur(overlayId: string): boolean {
         <div class="inertia-overlay">
             <template v-for="overlayId in stack" :key="overlayId">
                 <OverlayBackdrop
-                    :blur="shouldBlur(overlayId)"
+                    :blur="shouldBlurBackground(overlayId)"
                     @click="closeOverlay(overlayId)"
                 />
                 <Overlay :id="overlayId"/>
